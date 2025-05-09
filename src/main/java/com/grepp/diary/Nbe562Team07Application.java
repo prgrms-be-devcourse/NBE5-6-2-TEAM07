a65f33dd1b37c7c3d3f2d8211d30b5c008f69db2
IDIOcoder <newon.jsw@gmail.com>
2025-05-09 10:13:35 +0900

    init

---
+++ src/main/java/com/grepp/diary/Nbe562Team07Application.java
@@ -0,0 +1,13 @@
+package com.grepp.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe562Team07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbe562Team07Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/grepp/diary/Nbe562Team07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grepp.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe562Team07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
