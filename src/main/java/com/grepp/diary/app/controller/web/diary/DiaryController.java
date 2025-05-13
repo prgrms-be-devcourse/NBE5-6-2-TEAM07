da4d585c420580b61ac8b9c639157ba929f75033
Kimoon J <jkm3093@naver.com>
2025-05-13 17:36:54 +0900

    merge
    # Conflicts:
    #	src/main/java/com/grepp/diary/app/model/diary/DiaryService.java
    #	src/main/java/com/grepp/diary/app/model/diary/entity/DiaryImg.java
    #	src/main/resources/application.properties

--- src/main/java/com/grepp/diary/app/controller/web/diary/DiaryController.java
+++ src/main/java/com/grepp/diary/app/controller/web/diary/DiaryController.java
@@ -3,6 +3,7 @@ package com.grepp.diary.app.controller.web.diary;
 import com.grepp.diary.app.controller.web.diary.payload.DiaryRequest;
 import com.grepp.diary.app.model.diary.DiaryService;
 import com.grepp.diary.app.model.keyword.KeywordService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,7 +27,7 @@ public class DiaryController {
     public String showDiaryWritePage(Model model) {
         model.addAttribute("diaryRequest", new DiaryRequest());
         model.addAttribute("allKeywords", keywordService.findAllKeywords());
-        return "diary";
+        return "diary/diary";
     }
 
 //    @PostMapping
@@ -35,13 +37,14 @@ public class DiaryController {
 //    }
 
     @PostMapping
-    public String submitDiary(@ModelAttribute("diaryRequest") DiaryRequest form
+    public String writeAndSaveDiary(@ModelAttribute("diaryRequest") DiaryRequest form
         //@AuthenticationPrincipal CustomUserDetails userDetails
     ) {
         String userId = "user01";
-//        diaryService.saveDiary(form, userDetails.getMember());
 
+//        diaryService.saveDiary(form, userDetails.getMember());
         diaryService.saveDiary(form, userId);
+        //diaryService.saveDiaryImg(form);
         return "index"; // 작성 완료 후 목록 페이지로 이동
     }
 
--- src/main/java/com/grepp/diary/app/model/keyword/repository/KeywordRepository.java
+++ src/main/java/com/grepp/diary/app/model/keyword/repository/KeywordRepository.java
@@ -1,8 +1,18 @@
 package com.grepp.diary.app.model.keyword.repository;
 
 import com.grepp.diary.app.model.keyword.entity.Keyword;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface KeywordRepository extends JpaRepository<Keyword, Integer>, KeywordRepositoryCustom {
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+
+    Optional<Keyword> findByName(String name);
+
 
 }
