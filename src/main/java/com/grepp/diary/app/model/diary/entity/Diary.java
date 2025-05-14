c5a21afb532db3a7a34501583b356e9aa202ce28
Kimoon J <jkm3093@naver.com>
2025-05-14 22:56:42 +0900

    🧩 Entity GeneratedValue strategy

--- src/main/java/com/grepp/diary/app/model/diary/entity/Diary.java
+++ src/main/java/com/grepp/diary/app/model/diary/entity/Diary.java
@@ -29,7 +29,7 @@ import lombok.ToString;
 public class Diary extends BaseEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer diaryId;
     @Enumerated(EnumType.STRING)
     private Emotion emotion;
