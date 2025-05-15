5466928b4cf6833f799e97534bc30e8fe17f1b02
Kimoon J <jkm3093@naver.com>
2025-05-15 11:26:04 +0900

    ✨ Add diary writing #2

--- src/main/java/com/grepp/diary/app/model/diary/DiaryService.java
+++ src/main/java/com/grepp/diary/app/model/diary/DiaryService.java
@@ -61,8 +61,7 @@ public class DiaryService {
         LocalDateTime endDateTime = end.plusDays(1).atStartOfDay();
         return diaryRepository.findByMemberUserIdAndCreatedAtBetweenAndIsUseTrue(userId,
                                                                                  startDateTime, endDateTime);
-        return diaryRepository.findByMemberUserIdAndCreatedAtBetweenAndIsUseTrue(userId,
-            startDateTime, endDateTime);
+
     }
 
     public Integer getMonthDiariesCount() {
@@ -81,55 +80,6 @@ public class DiaryService {
         Pageable limit = PageRequest.of(page, size);
         return diaryRepository.findRecentDiariesWithImages(userId, limit);
     }
-
-    public Diary getDiaryById(int diaryId) {
-        Optional<Diary> result = diaryRepository.findById(diaryId);
-        if (result.isEmpty()) {
-            throw new RuntimeException("일기가 존재하지 않습니다.");
-        }
-        return result.get();
-    }
-
-    public DiaryDto getDto(int diaryId) {
-        Optional<Diary> result = diaryRepository.findById(diaryId);
-        if (result.isEmpty()) {
-            throw new RuntimeException("일기가 존재하지 않습니다.");
-        }
-        return DiaryDto.fromEntity(result.get());
-    }
-
-    public List<DiaryDto> getNoReplyDtos() {
-        List<Diary> diaries = diaryRepository.findAll();
-        List<Diary> NoReplyDiaries = diaries.stream().filter(e -> e.getReply() == null).toList();
-        return NoReplyDiaries.stream().map(DiaryDto::fromEntity).toList();
-    }
-
-    @Transactional
-    public void registReply(int diaryId, String replyContent) {
-        Optional<Diary> result = diaryRepository.findById(diaryId);
-        if (result.isEmpty()) {
-            throw new RuntimeException("일기가 존재하지 않습니다.");
-        }
-
-        Diary diary = result.get();
-        Reply reply = diary.getReply();
-        Member member = diary.getMember();
-        Custom custom = member.getCustom();
-        Ai ai = custom.getAi();
-
-        if (reply == null) { // 등록된 reply X
-            reply = new Reply();
-            reply.setContent(replyContent);
-            reply.setAi(ai);
-            reply.setDiary(diary);
-            diary.setReply(reply);
-        } else { // 등록된 reply O
-            reply.setContent(replyContent);
-            reply.setAi(ai);
-        }
-        diaryRepository.save(diary);
-    }
-
 
     public Diary getDiaryById(int diaryId) {
         Optional<Diary> result = diaryRepository.findById(diaryId);
