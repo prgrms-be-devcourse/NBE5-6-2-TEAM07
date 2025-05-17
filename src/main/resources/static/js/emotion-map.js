87381b7b1093143bb1dbb9e4b8ce82c46444efaf
IDIOcoder <newon.jsw@gmail.com>
2025-05-17 16:40:40 +0900

    :recycle: Refactor: Expose image map globally via window object

--- src/main/resources/static/js/emotion-map.js
+++ src/main/resources/static/js/emotion-map.js
@@ -1,5 +1,5 @@
 //TODO : 혹시 나중에 테마별 이미지셋이 변경되도록 된다면 이 부분 수정할 것
-const emotionImageMap = {
+window.emotionImageMap = {
   GOOD: "Good_Cloud_Sun.png",
   VERY_GOOD: "Very_Good_Sun.png",
   NORMAL: "Normal_Cloud.png",
