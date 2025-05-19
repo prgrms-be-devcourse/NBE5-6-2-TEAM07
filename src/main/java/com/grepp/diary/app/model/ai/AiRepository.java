3e19fd91079a736732f49a73052026a6e848ecaa
eonwy <yeawon6566@gmail.com>
2025-05-19 11:12:35 +0900

    🐛fix ai repository path

--- src/main/java/com/grepp/diary/app/model/ai/AiRepository.java
+++
@@ -1,8 +0,0 @@
-package com.grepp.diary.app.model.ai;
-
-import com.grepp.diary.app.model.ai.entity.Ai;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AiRepository extends JpaRepository<Ai, Integer> {
-
-}
