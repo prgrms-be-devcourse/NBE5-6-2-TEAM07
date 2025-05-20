eafb9c357b70c0bc4f55ec65ecf58b9b524deb59
IDIOcoder <newon.jsw@gmail.com>
2025-05-20 17:53:39 +0900

    :bug: Fix calendar click bug causing wrong diary date to be opened

--- src/main/java/com/grepp/diary/app/model/diary/repository/DiaryRepository.java
+++ src/main/java/com/grepp/diary/app/model/diary/repository/DiaryRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DiaryRepository extends JpaRepository<Diary, Integer>, DiaryRepositoryCustom {
     List<Diary> findByMemberUserIdAndDateBetweenAndIsUseTrueOrderByDateAsc(String userId, LocalDate start, LocalDate end);
+    List<Diary> findByMemberUserIdAndDateAndIsUseTrue(String userId, LocalDate date);
 
     Integer countByCreatedAtBetweenAndIsUseTrue(LocalDateTime startDateTime, LocalDateTime endDateTime);
     Integer countByMemberUserIdAndCreatedAtBetweenAndIsUseTrue(String userId, LocalDateTime start, LocalDateTime end);
