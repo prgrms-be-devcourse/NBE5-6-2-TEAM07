bfa2d14dc4246380b1a7974b0255063982048a6b
Kimoon J <jkm3093@naver.com>
2025-05-21 15:07:24 +0900

    ♻️ Replace hardcoded username with SecurityContext user

--- src/main/resources/static/js/diary.js
+++ src/main/resources/static/js/diary.js
@@ -27,7 +27,7 @@ dateInput.addEventListener('change', async() => {
 
   const dateParam = dateInput.value;
   try {
-    const res = await fetch(`/api/diary/check?userId=user01&date=${dateParam}`);
+    const res = await fetch(`/api/diary/check?date=${dateParam}`);
     const exists = await res.json();
 
     if (exists) {
